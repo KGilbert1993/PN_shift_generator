dc125f422573135d0062d8a73731bc90f538123d
Kevin Gilbert <pompeii@vulcan.(none)>
2014-10-23 12:22:32 -0500

    Added input bounds check

--- pn_gen.c
+++ pn_gen.c
@@ -6,6 +6,14 @@ int main(int argc, char **argv) {
 	int internal_state = 0x05;
 	int i, feedback;
 
+	if(argc < 2) {
+		printf("Please enter number of cycles to simulate\n");
+		return 1;
+	}
+	if(atoi(argv[1]) > 255) {
+		printf("Please enter a number less than 255\n");
+		return 2;
+	}
 	for(i=0; i<(atoi(argv[1]));i++) {
 		out[i] = (internal_state & 0x01);
 		feedback = (out[i] ^ ((internal_state & 0x08)>>3)) << 5;
