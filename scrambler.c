6bcf902875732d67ecaf43ea4ab81bb37e40ea20
Kevin Gilbert <pompeii@vulcan.(none)>
2014-10-23 13:18:01 -0500

    Scrambler and unscrambler file

---
+++ scrambler.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SCRAMBLEROUT
+
+int main(int argc, char **argv) {
+	int out[255];
+	int out2[255];
+	int internal_state = 0x05;
+	int unscrambler = 0x05;
+	int i, feedback;
+	int input = 1;
+
+	if(argc < 2) {
+		printf("Please enter number of cycles to simulate\n");
+		return 1;
+	}
+	if(atoi(argv[1]) > 255) {
+		printf("Please enter a number less than 255\n");
+		return 2;
+	}
+	for(i=0; i<(atoi(argv[1]));i++) {
+		feedback = (input ^ ((internal_state&0x01) ^ ((internal_state & 0x08)>>3)));
+		out[i] = feedback;
+		feedback <<= 5;
+		internal_state &= 0x1F;
+		internal_state |= feedback;
+		internal_state >>= 1;
+		// scrambled
+
+		unscrambler &= 0x1F;
+		unscrambler |= feedback;
+		
+		out2[i] = ((unscrambler & 0x08)>>3) ^ ((unscrambler & 0x20) >> 5);
+		unscrambler >>= 1;		
+
+	#ifdef SCRAMBLEROUT
+		printf("%0.2d --> %d | internal state:%0.2d\n",i,out[i],internal_state);
+	#else
+		printf("%0.2d --> %d | internal state; %0.2d\n",i,out2[i],unscrambler);
+	#endif
+	}
+}
